d25e2403bd6554b0a861012f02d7103f32f0360a
Héric Gomes da Silva <h1g2s0@hotmail.com>
2024-10-24 15:46:04 -0300

    JS clima

--- src/js/index.js
+++ src/js/index.js
@@ -2,29 +2,40 @@ const chaveDaApi = "1f8421d36db24a9bba4134848241710";
 
 const botaoDeBusca = document.querySelector(".btn-busca");
 
-botaoDeBusca.addEventListener("click",()=>{
+botaoDeBusca.addEventListener("click", async () => {
     const cidade = document.getElementById("input-busca").value;
-    const dados = buscarDadosDaCidade(cidade);
+    
+    if(!cidade) return;
+    
+    const dados = await buscarDadosDaCidade(cidade);
 
-    preencherDadosNaTela(dados, cidade);
-    console.log(botaoDeBusca);
-
+    if(dados) dadosNaTela(dados, cidade);
+   
 });
 
 async function buscarDadosDaCidade(cidade) {
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=nolang=pt`;
-
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+    
     const resposta = await fetch(apiUrl);
 
-    const dados = resposta.jason();
+    if(resposta.status !==200) return;
+
+    const dados = resposta.json();
+    
     return dados;
 }
 
-function preencherDadosNaTela(dados, cidade){
-
+function dadosNaTela(dados, cidade) {
     const temperatura = dados.current.temp_c;
+    const condicao = dados.current.condition.text;
+    const humidade = dados.current.humidity;
+    const velocidadeDoVento = dados.current.wind_kph;
+    const iconeCondicao = dados.current.condition.icon;
+
     document.getElementById("cidade").textContent = cidade;
-    document.getElementById("temperatura").textContent = `${temperatura}°C`;
-
+    document.getElementById("temperatura").textContent = `${temperatura} °C`;
+    document.getElementById("condicao").textContent = condicao;
+    document.getElementById("humidade").textContent = `${humidade}%`;
+    document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento}km/h`;
+    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
 }
-
